554d6e896aa66f79f945208e3bc0d01b57761c03
kuroneko konoha <hayasaka.ryosuke@gmail.com>
2013-12-13 01:30:40 +0900

    bugfix:jsondownload interval

--- main.rb
+++ main.rb
@@ -42,26 +42,20 @@ def todayfileanalysis()
 end
 
 def pastfileanalysis(dateyear,datemonth,dateday)
-	if((DateTime.now.to_time-File.stat("./juyo-j.csv").mtime.to_time).to_i > 0) then
-		filename=dateyear.to_s+'-'+datemonth.to_s+'-'+dateday.to_s+'.json'
+	filename=dateyear.to_s+'-'+datemonth.to_s+'-'+dateday.to_s+'.json'
+	if(File.exist?(filename)==false) then
 		url='http://tepco-usage-api.appspot.com/'+dateyear.to_s+'/'+datemonth.to_s+'/'+dateday.to_s+'.json'
 		filedownload(url,filename)
 		powerdata=[]
-		count = 0
-		datatime=""
 		jsondata=open(filename).read
-		p jsondata
 		jsonpowerdata = JSON.parser.new(jsondata).parse()
 		jsonpowerdata.each do |data|
 			powerdata << data['usage'].to_f*10000/10**6#kWはわかりにくいのでMW
-			p data['usage'].to_f*10000/10**6#kWはわかりにくいのでMW
-
 		end
+		xlabels={0 => '0',1 => '1',2 => '2',3 => '3',4 => '4',5 => '5',6 => '6',7 => '7',8 => '8',9 => '9',10 => '10',11 => '11',12 => '12',13 => '13',14 => '14',15 => '15',16 => '16',17 => '17',18 => '18',19 => '19',20 => '20',21 => '21',22 => '22',23 => '23'}
+		graphgenerate(xlabels,powerdata,'Hour','Power Usage[MW]','PowerUsageGraph'+dateyear.to_s+'-'+datemonth.to_s+'-'+dateday.to_s)
 	end
-	xlabels={0 => '0',1 => '1',2 => '2',3 => '3',4 => '4',5 => '5',6 => '6',7 => '7',8 => '8',9 => '9',10 => '10',11 => '11',12 => '12',13 => '13',14 => '14',15 => '15',16 => '16',17 => '17',18 => '18',19 => '19',20 => '20',21 => '21',22 => '22',23 => '23'}
-	graphgenerate(xlabels,powerdata,'Hour','Power Usage[MW]','PowerUsageGraph'+dateyear.to_s+'-'+datemonth.to_s+'-'+dateday.to_s)
 	return ""
-
 end
 
 set :bind, '0.0.0.0'
